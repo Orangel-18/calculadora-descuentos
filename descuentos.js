07c3c9c25d34acdd3e50b8d265f3de9bc8e99e61
Orangel <orangabriel18@gmail.com>
2022-04-25 12:59:54 -0400

    initial commit

---
+++ descuentos.js
@@ -0,0 +1,16 @@
+function calcularPrecio(precio, descuento) {
+    const factor = (100 - descuento) / 100;
+    return (precio * factor);
+}
+
+function precioConDescuento() {
+    const inputPrecio = document.getElementById("precio").value;
+    const inputDescuento = document.getElementById("descuento").value;
+
+    const precio = inputPrecio;
+    const descuento = inputDescuento;
+
+    const porPagar = calcularPrecio(precio, descuento);
+
+    alert(`El total a pagar es de $ ${porPagar}`);
+}
